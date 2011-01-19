470aa3a92785de61a9141213aa2302bc4e2765a9
Adam Wolfe Gordon <awg@xvx.ca>
2011-01-18 21:49:42 -0700

    Welcome screen on first run with instructions about the settings.

--- src/ca/xvx/tracks/SettingsActivity.java
+++ src/ca/xvx/tracks/SettingsActivity.java
@@ -2,6 +2,8 @@ package ca.xvx.tracks;
 
 import android.util.Log;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
@@ -14,14 +16,29 @@ public class SettingsActivity extends PreferenceActivity {
 	protected void onCreate(Bundle savedState) {
 		super.onCreate(savedState);
 
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(!prefs.getBoolean(PreferenceConstants.RUN, false)) {
+			prefs.edit().putBoolean(PreferenceConstants.RUN, true).commit();
+			AlertDialog.Builder b = new AlertDialog.Builder(this);
+			b.setCancelable(false)
+				.setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface d, int id) {
+							d.cancel();
+						}
+					})
+				.setMessage(R.string.MSG_welcome)
+				.show();
+		}
+		
 		try {
 			addPreferencesFromResource(R.xml.preferences);
 		} catch(ClassCastException e) {
 			// Blow away prefs
 			Log.i(TAG, "Clearing preferences because we couldn't load them");
-			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 			SharedPreferences.Editor ed = prefs.edit();
 			ed.clear();
+			ed.putBoolean(PreferenceConstants.RUN, true);
 			ed.commit();
 
 			PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
